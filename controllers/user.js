1db7b4a5c5707eb934c87308249e155a570a91de
dk23 <james_k23@hotmail.gr>
2020-04-15 21:43:41 +0300

    user controller code refactored

--- controllers/user.js
+++ controllers/user.js
@@ -34,54 +34,144 @@ exports.getAllUsers = async (req, res) => {
     }
 };
 
-exports.update = (req, res) => {
-    User.findOneAndUpdate(
-        { _id: req.profile._id },
-        { $set: req.body },
-        { new: true },
-        (err, user) => {
-            if (err) {
-                return res.status(400).json({
-                    error: 'You are not authorized to perform this action',
-                });
-            }
+exports.update = async (req, res) => {
+    try {
+        let user = req.user;
 
-            req.hashed_password = undefined;
-            req.salt = undefined;
-            res.json(user);
-        }
-    );
+        await user.update({ $set: req.body }, { new: true });
+
+        user.salt = undefined;
+        user.hashed_password = undefined;
+
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
+    // User.findOneAndUpdate(
+    //     { _id: req.profile._id },
+    //     { $set: req.body },
+    //     { new: true },
+    //     (err, user) => {
+    //         if (err) {
+    //             return res.status(400).json({
+    //                 error: 'You are not authorized to perform this action',
+    //             });
+    //         }
+
+    //         req.hashed_password = undefined;
+    //         req.salt = undefined;
+    //         res.json(user);
+    //     }
+    // );
 };
 
-exports.addCommentToUserHistory = (req, res, next) => {
-    let history = [];
+// In frontend, user history will show every action of the user: new post, comment, and reply
+exports.addPostToUserHistory = async (req, res, next) => {
+    try {
+        let user = req.user;
 
-    req.body.comments.forEach(({ _id, _postId, commentText, category }) => {
-        history.push({
-            _id,
-            _postId,
-            commentText,
-            category,
+        user.history.unshift({
+            id: req.post._id,
+            parentId: null,
+            type: 'post',
+            parent: null,
         });
-    });
+
+        await user.save();
+
+        next();
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
 };
 
-exports.resetUserPassword = (req, res, next) => {
-    const { _id } = req.body;
-    console.log('_id ', _id);
-    User.findById(_id, (err, user) => {
-        if (err) {
-            return res.status(403).json({
-                error: 'You are not authorized to perform this action',
-            });
+exports.addCommentToUserHistory = async (req, res, next) => {
+    try {
+        let user = req.user;
+
+        user.history.unshift({
+            id: req.comment._id,
+            parentId: req.post._id,
+            type: 'comment',
+            parent: 'post',
+        });
+
+        await user.save();
+
+        next();
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
+};
+
+exports.addReplyToUserHistory = async (req, res, next) => {
+    try {
+        let user = req.user;
+
+        user.history.unshift({
+            id: req.reply._id,
+            parentId: req.comment._id,
+            type: 'reply',
+            parent: 'comment',
+        });
+
+        await user.save();
+
+        next();
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
+};
+
+exports.deleteActionFromUserHistory = async (req, res, action, next) => {
+    try {
+        let user = req.profile;
+        let removeIndex;
+        switch (action) {
+            case 'post':
+                removeIndex = user.history
+                    .map((action) => action.id.toString())
+                    .indexOf(req.post._id);
+                user.history.splice(removeIndex, 1);
+                break;
+            case 'comment':
+                removeIndex = user.history
+                    .map((action) => action.id.toString())
+                    .indexOf(req.comment._id);
+                user.history.splice(removeIndex, 1);
+                break;
+            case 'reply':
+                removeIndex = user.history
+                    .map((action) => action.id.toString())
+                    .indexOf(req.reply._id);
+                user.history.splice(removeIndex, 1);
+                break;
+            default:
+                break;
         }
 
-        console.log('user ', user);
+        await user.save();
+        next();
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
+};
+
+exports.resetUserPassword = async (req, res, next) => {
+    try {
+        const { _id } = req.body;
+        let user = await User.findById(_id);
+
         user.password = '123456';
-        user.save();
-
-        return res.json({
-            message: `Password of user with name ${user.name} has been reset!`,
-        });
-    });
+        await user.save();
+        res.json(user);
+    } catch (err) {
+        console.error(err.message);
+        return res.status(500).send('Server error');
+    }
 };
