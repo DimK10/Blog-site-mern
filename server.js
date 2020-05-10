3ed739663e51233f40b67cc7b342acf1382b5ace
DimK10 <james_k23@hotmail.gr>
2020-05-10 23:55:10 +0300

    Prepared for deployment

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const cookieParser = require('cookie-parser');
 const cors = require('cors');
 const expressValidator = require('express-validator');
 require('dotenv').config();
+const path = require('path');
 
 // Import routes
 const authRoutes = require('./routes/auth');
@@ -38,6 +39,16 @@ app.use('/api', categoryRoutes);
 app.use('/api', postRoutes);
 app.use('/api', commentRoutes);
 app.use('/api', replyRoutes);
+
+// Serve static assets in production
+if (process.env.NODE_ENV === 'production') {
+    // Set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const port = process.env.PORT || 8000;
 
